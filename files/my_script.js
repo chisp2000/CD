8b67b61fbda5447755a116cfd61fe9e9836cd75d
Bogdan <chisp2000@protonmail.com>
2023-05-31 23:31:28 -0400

    ADDED MAP

--- files/my_script.js
+++ files/my_script.js
@@ -2,38 +2,21 @@ function floatAndDisappear() {
   const planet = document.getElementById('gif');
   planet.style.animation = 'floatAndDisappear 1s forwards';
 
-  const projectsBody = document.getElementById('projectsBody');
-  if (projectsBody) {
-    projectsBody.classList.add('slide-up-and-fade-in');
-  }
-
   const navbar = document.getElementById('navbar');
   if (navbar) {
     navbar.classList.remove('hide');
   }
-}
 
-function showContact() {
-  const projectsBody = document.getElementById('projectsBody');
-  const contactBody = document.getElementById('contactBody');
+  const mapbody = document.getElementById('map');
 
-  if (projectsBody && contactBody) {
-    projectsBody.classList.remove('slide-up-and-fade-in');
-    projectsBody.classList.add('hide');
-    contactBody.classList.remove('hide');
-    contactBody.classList.add('slide-up-and-fade-in');
-  }
-}
+  if (mapbody) {
+    mapbody.classList.remove('hide');
+    mapbody.classList.add('slide-up-and-fade-in');
 
-function showProjects() {
-  const projectsBody = document.getElementById('projectsBody');
-  const contactBody = document.getElementById('contactBody');
-
-  if (projectsBody && contactBody) {
-    contactBody.classList.remove('slide-up-and-fade-in');
-    contactBody.classList.add('hide');
-    projectsBody.classList.remove('hide');
-    projectsBody.classList.add('slide-up-and-fade-in');
+    // Add event listener to wait for the animation to finish
+    planet.addEventListener('animationend', () => {
+      mapbody.style.display = 'block';
+    }, { once: true });
   }
 }
 
