a3b2a3948fddb3f2fbc97e796fed6ead7b56a9c4
Bogdan <chisp2000@protonmail.com>
2023-06-01 22:38:22 -0400

    Added menus that open when countries are clicked

--- files/my_script.js
+++ files/my_script.js
@@ -23,3 +23,62 @@ function floatAndDisappear() {
 function goToHome() {
   location.reload(); // Refresh the page
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Get references to the country elements
+  const norwayElement = document.querySelector('#Norway');
+  const ukraineElement = document.querySelector('#Ukraine');
+  const usaElement = document.querySelector('#USA');
+
+  // Get references to the window containers
+  const norwayWindow = document.querySelector('.NorwayWindow');
+  const ukraineWindow = document.querySelector('.UkraineWindow');
+  const usaWindow = document.querySelector('.USAWindow');
+
+  // Get references to the close buttons
+  const closeButtons = document.querySelectorAll('.close-button');
+
+  // Add event listeners to the country elements
+  norwayElement.addEventListener('click', openNorwayWindow);
+  ukraineElement.addEventListener('click', openUkraineWindow);
+  usaElement.addEventListener('click', openUSAWindow);
+
+  // Add event listeners to the close buttons
+  closeButtons.forEach(button => {
+    button.addEventListener('click', closeWindow);
+  });
+
+  // Function to open the Norway window
+  function openNorwayWindow() {
+    hideAllWindows();
+    norwayWindow.style.display = 'block';
+  }
+
+  // Function to open the Ukraine window
+  function openUkraineWindow() {
+    hideAllWindows();
+    ukraineWindow.style.display = 'block';
+  }
+
+  // Function to open the USA window
+  function openUSAWindow() {
+    hideAllWindows();
+    usaWindow.style.display = 'block';
+  }
+
+  // Function to hide all windows
+  function hideAllWindows() {
+    norwayWindow.style.display = 'none';
+    ukraineWindow.style.display = 'none';
+    usaWindow.style.display = 'none';
+  }
+
+  // Function to close the window
+  function closeWindow(event) {
+  const closeButton = event.target;
+  const windowContainer = closeButton.parentElement;
+  windowContainer.style.display = 'none';
+}
+
+});
